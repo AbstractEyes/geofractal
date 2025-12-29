6b00d478546784319414b10b2ce3f70eb3736e0b
AbstractPhil <philcoelho@live.com>
2025-12-29 15:44:11 -0800

    compiler improved - benchmark your models

--- src/geofractal/router/compiler/__init__.py
+++
@@ -1,6 +0,0 @@
-#from .compile_router import compiler as wide_compiler
-#
-#from .compile_benchmark import run_benchmark as wide_compiler_benchmark
-#
-#__all__ = ['wide_compiler', 'wide_compiler_benchmark']
-#
